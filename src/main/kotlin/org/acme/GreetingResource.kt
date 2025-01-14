ec5af2f8fcbacadc3d59b50e8e111942cd2a4d2a
velimir <velimir.mueller@googlemail.com>
2025-01-14 16:50:16 +0100

    initial commit

---
+++ src/main/kotlin/org/acme/GreetingResource.kt
@@ -0,0 +1,14 @@
+package org.acme
+
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+import org.acme.Greeting
+
+@Path("/hello")
+class GreetingResource {
+
+    @GET
+    fun hello() = Greeting("hello")
+}
