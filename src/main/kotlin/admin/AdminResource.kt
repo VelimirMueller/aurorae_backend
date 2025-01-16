75bf3b6765aa9b57260db63b8d4d55e8dd51ca57
velimir <velimir.mueller@googlemail.com>
2025-01-16 07:24:12 +0100

    add jakartka security and protected /admin route to AdmnResource.kt

--- src/main/kotlin/admin/AdminResource.kt
+++ src/main/kotlin/admin/AdminResource.kt
@@ -7,13 +7,16 @@ import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
 import org.acme.Greeting
 import jakarta.annotation.security.PermitAll
+import jakarta.annotation.security.RolesAllowed
 
 @Path("/admin")
 class AdminResource {
 
     @GET
+    @RolesAllowed("admin")
     fun admin() = Greeting("Hello admin")
 
     @POST
+    @RolesAllowed("admin")
     fun adminPost() = Greeting("Hello from admin api")
 }
