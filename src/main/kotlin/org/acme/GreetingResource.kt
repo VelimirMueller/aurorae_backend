0a2d5d7cb8c365aa0d2e4a5ce6a6ceb50228dcc9
velimir <velimir.mueller@googlemail.com>
2025-01-17 22:29:24 +0100

    maintainability: fix sonarcloud issue (Remove this redundant import.)

--- src/main/kotlin/org/acme/GreetingResource.kt
+++ src/main/kotlin/org/acme/GreetingResource.kt
@@ -5,7 +5,6 @@ import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
-import org.acme.Greeting
 
 @Path("/hello")
 class GreetingResource {
