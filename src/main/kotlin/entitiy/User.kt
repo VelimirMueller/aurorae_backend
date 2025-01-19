b4bcf511e8959490a9a3baed18714cd8647073e9
velimir <velimir.mueller@googlemail.com>
2025-01-19 20:53:29 +0100

    add user entity

--- src/main/kotlin/entitiy/User.kt
+++ src/main/kotlin/entitiy/User.kt
@@ -1,8 +1,5 @@
 package entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
-
 import io.quarkus.hibernate.orm.panache.PanacheEntity
 import io.quarkus.elytron.security.common.BcryptUtil
 import io.quarkus.security.jpa.Password
@@ -10,9 +7,8 @@ import io.quarkus.security.jpa.Roles
 import io.quarkus.security.jpa.UserDefinition
 import io.quarkus.security.jpa.Username
 
-@Entity
-@Table(name = "user_t")
-@UserDefinition
-public class User extends PanacheEntity {
-   
+class Person: PanacheEntity() {
+    lateinit var name: String
+    lateinit var email: String
+    lateinit var role: String
 }
